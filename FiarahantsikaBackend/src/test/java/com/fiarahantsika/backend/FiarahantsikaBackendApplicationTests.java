d1657c0e0c325675b7219c1c62bb20fe0a6778d9
Arinomena <nomena.ratsira@gmail.com>
2025-07-03 19:59:11 +0300

    Init Spring Boot backend – project structure and config

---
+++ FiarahantsikaBackend/src/test/java/com/fiarahantsika/backend/FiarahantsikaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiarahantsika.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiarahantsikaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
